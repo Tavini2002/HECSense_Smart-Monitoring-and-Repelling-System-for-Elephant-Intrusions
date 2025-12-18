6e2b6d07be78252fed82202fa4e1fd78242e9de8
IT22182128 <IT22182128@my.sliit.lk>
2025-12-18 13:04:02 +0530

    Update: change the setting page

--- experiment/web-app/resources/views/settings.blade.php
+++ experiment/web-app/resources/views/settings.blade.php
@@ -1,218 +1,208 @@
 @extends('layouts.dashboard_master_layout')
 
 @section('HeaderAssets')
+    <title>IOV | Account Settings</title>
 
-<title>IOV - Settings</title>
+    {{-- Datepicker --}}
+    <link rel="stylesheet" href="{{ asset('assets/plugins/bootstrap-datepicker/css/bootstrap-datepicker.min.css') }}">
 
-<link href="{{asset('assets')}}/plugins/bootstrap-datepicker/css/bootstrap-datepicker.min.css" rel="stylesheet">
-
-<link href="{{asset('assets')}}/css/bootstrap.min.css" rel="stylesheet" type="text/css">
-<link href="{{asset('assets')}}/css/metismenu.min.css" rel="stylesheet" type="text/css">
-<link href="{{asset('assets')}}/css/icons.css" rel="stylesheet" type="text/css">
-<link href="{{asset('assets')}}/css/style.css" rel="stylesheet" type="text/css">
-
+    {{-- Core styles --}}
+    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/metismenu.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/icons.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
 @endsection
 
 
 @section('PageContent')
-
-
 <div class="container-fluid">
-    <div class="page-title-box">
 
-        <div class="row align-items-center ">
+    {{-- Page Header --}}
+    <div class="page-title-box">
+        <div class="row align-items-center">
             <div class="col-md-8">
-                <div class="page-title-box">
-                    <h4 class="page-title">Credentials Settings</h4>
-                    <ol class="breadcrumb">
-                        <li class="breadcrumb-item">
-                            <a href="javascript:void(0);">IOV</a>
-                        </li>
-                        <li class="breadcrumb-item">
-                            <a href="#">Settings</a>
-                        </li>
-                        <li class="breadcrumb-item active" style="text-transform: capitalize;">Credentials</li>
-                    </ol>
-                </div>
+                <h4 class="page-title">Credential Management</h4>
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="javascript:void(0);">IOV</a></li>
+                    <li class="breadcrumb-item"><a href="#">Settings</a></li>
+                    <li class="breadcrumb-item active text-capitalize">Credentials</li>
+                </ol>
             </div>
         </div>
     </div>
 
+    {{-- Alerts --}}
     <div class="row">
-
-
         <div class="col-12">
 
             @if ($errors->any())
-                <div class="alert alert-danger" style="background-color: red; color: white; font-size: 18px; text-align: center;">
-                    <button type="button" class="close" data-dismiss="alert" aria-label="Close" style="color:white;">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                    <ul>
-                        @foreach ($errors->all() as $error)
-                            <li>{{ $error }}</li>
+                <div class="alert alert-danger text-center" style="background:red;color:#fff;font-size:18px;">
+                    <button type="button" class="close text-white" data-dismiss="alert">&times;</button>
+                    <ul class="mb-0">
+                        @foreach ($errors->all() as $err)
+                            <li>{{ $err }}</li>
                         @endforeach
                     </ul>
                 </div>
             @endif
 
             @if (session('error'))
-                <div class="alert alert-danger" style="background-color: red; color: white; font-size: 18px; text-align: center;">
-                    <button type="button" class="close" data-dismiss="alert" aria-label="Close" style="color:white;">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
+                <div class="alert alert-danger text-center" style="background:red;color:#fff;font-size:18px;">
+                    <button type="button" class="close text-white" data-dismiss="alert">&times;</button>
                     {{ session('error') }}
                 </div>
             @endif
 
             @if (session('success'))
-                <div class="alert alert-success" style="background-color: green; color: white; font-size: 18px; text-align: center;">
-                    <button type="button" class="close" data-dismiss="alert" aria-label="Close" style="color:white;">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
+                <div class="alert alert-success text-center" style="background:green;color:#fff;font-size:18px;">
+                    <button type="button" class="close text-white" data-dismiss="alert">&times;</button>
                     {{ session('success') }}
                 </div>
             @endif
 
         </div>
-
-        <!-- Change Username Form -->
-        <div class="col-6">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title text-center mb-4">Change Username</h5>
-                    <hr>
-                    <form id="updateUsernameForm" action="{{ route('update.username') }}" method="POST" onsubmit="confirmUpdate(event)">
-                        @csrf
-                        <div class="form-group">
-                            <label for="username">Username:</label>
-                            <input type="text" class="form-control" id="username" name="username" value="{{ $username[0] }}" placeholder="Enter the username" oninput="checkUsernameChange()">
-                        </div>
-                        
-                        <div class="text-right">
-                            <button class="btn btn-secondary" type="submit" id="updateButton" disabled>Change</button>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-        
-        
-        <!-- Change Password Form -->
-        <div class="col-6">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title text-center mb-4">Change Password</h5>
-                    <hr>
-                    <form id="changePasswordForm" action="{{ route('update.password') }}" method="POST">
-                        @csrf
-                        <div class="form-group">
-                            <label for="current_password">Current Password:</label>
-                            <input type="password" class="form-control" id="current_password" name="current_password" placeholder="Enter current password" required>
-                        </div>
-                        
-                        <div class="form-group">
-                            <label for="new_password">New Password:</label>
-                            <input type="password" class="form-control" id="new_password" name="new_password" placeholder="Enter new password" required>
-                        </div>
-                        
-                        <div class="form-group">
-                            <label for="new_password_confirmation">Confirm Password:</label>
-                            <input type="password" class="form-control" id="new_password_confirmation" name="new_password_confirmation" placeholder="Confirm new password" required>
-                        </div>
-                        
-                        <div class="text-right">
-                            <button class="btn btn-info" type="submit">Change</button>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-
-        
-        
     </div>
-    
 
+    {{-- Forms --}}
+    <div class="row">
+
+        {{-- Username Update --}}
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="text-center mb-3">Update Username</h5>
+                    <hr>
+
+                    <form id="usernameUpdateForm"
+                          action="{{ route('update.username') }}"
+                          method="POST"
+                          onsubmit="handleUsernameConfirm(event)">
+                        @csrf
+
+                        <div class="form-group">
+                            <label>Username</label>
+                            <input type="text"
+                                   class="form-control"
+                                   name="username"
+                                   id="usernameInput"
+                                   value="{{ $username[0] }}"
+                                   placeholder="Enter username"
+                                   oninput="toggleUsernameButton()">
+                        </div>
+
+                        <div class="text-right">
+                            <button type="submit"
+                                    id="usernameSubmitBtn"
+                                    class="btn btn-secondary"
+                                    disabled>
+                                Update
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+
+        {{-- Password Update --}}
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="text-center mb-3">Update Password</h5>
+                    <hr>
+
+                    <form action="{{ route('update.password') }}" method="POST">
+                        @csrf
+
+                        <div class="form-group">
+                            <label>Current Password</label>
+                            <input type="password" name="current_password" class="form-control" required>
+                        </div>
+
+                        <div class="form-group">
+                            <label>New Password</label>
+                            <input type="password" name="new_password" class="form-control" required>
+                        </div>
+
+                        <div class="form-group">
+                            <label>Confirm Password</label>
+                            <input type="password" name="new_password_confirmation" class="form-control" required>
+                        </div>
+
+                        <div class="text-right">
+                            <button type="submit" class="btn btn-info">Update</button>
+                        </div>
+                    </form>
+
+                </div>
+            </div>
+        </div>
+
+    </div>
 
 </div>
-
-
 @endsection
 
 
 @section('FooterAssets')
 
-    <!-- jQuery  -->
-    <script src="{{asset('assets')}}/js/jquery.min.js"></script>
-    <script src="{{asset('assets')}}/js/bootstrap.bundle.min.js"></script>
-    <script src="{{asset('assets')}}/js/metismenu.min.js"></script>
-    <script src="{{asset('assets')}}/js/jquery.slimscroll.js"></script>
-    <script src="{{asset('assets')}}/js/waves.min.js"></script>
+    {{-- Core scripts --}}
+    <script src="{{ asset('assets/js/jquery.min.js') }}"></script>
+    <script src="{{ asset('assets/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('assets/js/metismenu.min.js') }}"></script>
+    <script src="{{ asset('assets/js/jquery.slimscroll.js') }}"></script>
+    <script src="{{ asset('assets/js/waves.min.js') }}"></script>
 
-    <script src="{{asset('assets')}}/plugins/bootstrap-datepicker/js/bootstrap-datepicker.min.js"></script>
+    <script src="{{ asset('assets/plugins/bootstrap-datepicker/js/bootstrap-datepicker.min.js') }}"></script>
+    <script src="{{ asset('assets/plugins/sweet-alert2/sweetalert2.all.min.js') }}"></script>
+    <script src="{{ asset('assets/js/app.js') }}"></script>
 
-    <!-- App js -->
-    <script src="{{asset('assets')}}/js/app.js"></script>
-
-    <script src="{{asset('assets')}}/plugins/sweet-alert2/sweetalert2.all.min.js"></script>
-
+    {{-- Auto-hide alerts --}}
     <script>
-        $(document).ready(function() {
-            setTimeout(function() {
-                $('.alert').fadeOut('slow');
-            }, 6000); // 6 seconds
+        $(function () {
+            setTimeout(() => $('.alert').fadeOut('slow'), 6000);
         });
     </script>
 
+    {{-- Username confirmation --}}
     <script>
-        function confirmUpdate(event) {
-            event.preventDefault(); // Prevent the default form submission
-        
+        function handleUsernameConfirm(e) {
+            e.preventDefault();
+
             Swal.fire({
-                title: "Are you sure?",
-                text: "Do you want to update the username?",
-                icon: "warning",
+                title: 'Confirm Action',
+                text: 'Proceed with username update?',
+                icon: 'warning',
                 showCancelButton: true,
-                background: "#12192b",
-                color: "#fff",
-                confirmButtonColor: "#fb4365",
-                cancelButtonColor: "#20d4b6",
-                confirmButtonText: "Yes, update it!",
-                cancelButtonText: "No, cancel!",
-                customClass: {
-                    popup: 'custom-swal-popup'
-                }
-            }).then((result) => {
-                if (result.isConfirmed) {
-                    // If confirmed, submit the form for updating username
-                    document.getElementById('updateUsernameForm').submit();
+                background: '#12192b',
+                color: '#ffffff',
+                confirmButtonColor: '#fb4365',
+                cancelButtonColor: '#20d4b6',
+                confirmButtonText: 'Yes, proceed',
+                cancelButtonText: 'Cancel'
+            }).then((res) => {
+                if (res.isConfirmed) {
+                    document.getElementById('usernameUpdateForm').submit();
                 }
             });
         }
     </script>
 
+    {{-- Username change detection --}}
     <script>
-        // Store the original username value
-        const originalUsername = "{{ $username[0] }}";
-        
-        function checkUsernameChange() {
-            const usernameField = document.getElementById('username');
-            const updateButton = document.getElementById('updateButton');
-            
-            // Check if the current value differs from the original username
-            if (usernameField.value.trim() !== originalUsername) {
-                updateButton.disabled = false;
-                updateButton.classList.remove('btn-secondary');
-                updateButton.classList.add('btn-info');
+        const initialUsername = "{{ $username[0] }}";
+
+        function toggleUsernameButton() {
+            const input = document.getElementById('usernameInput');
+            const btn = document.getElementById('usernameSubmitBtn');
+
+            if (input.value.trim() !== initialUsername) {
+                btn.disabled = false;
+                btn.classList.replace('btn-secondary', 'btn-info');
             } else {
-                updateButton.disabled = true;
-                updateButton.classList.remove('btn-info');
-                updateButton.classList.add('btn-secondary');
+                btn.disabled = true;
+                btn.classList.replace('btn-info', 'btn-secondary');
             }
         }
-
     </script>
-
 
 @endsection
