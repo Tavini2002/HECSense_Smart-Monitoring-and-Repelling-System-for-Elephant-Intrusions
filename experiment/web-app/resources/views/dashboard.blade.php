bde39adf2a0c55d2e73a97be961093e80b418bfc
IT22182128 <IT22182128@my.sliit.lk>
2025-12-18 13:01:30 +0530

    Feat: update the dashboard screen

--- experiment/web-app/resources/views/dashboard.blade.php
+++ experiment/web-app/resources/views/dashboard.blade.php
@@ -1,57 +1,61 @@
 @extends('layouts.dashboard_master_layout')
 
 @section('HeaderAssets')
-  <title>Dashboard</title>
+    <title>Dashboard</title>
 
-  <!-- DataTables -->
-  <link href="{{asset('assets')}}/plugins/datatables/dataTables.bootstrap4.min.css" rel="stylesheet" type="text/css" />
-  <link href="{{asset('assets')}}/plugins/datatables/buttons.bootstrap4.min.css" rel="stylesheet" type="text/css" />
-  <link href="{{asset('assets')}}/plugins/datatables/responsive.bootstrap4.min.css" rel="stylesheet" type="text/css" />
+    {{-- DataTables styles --}}
+    <link rel="stylesheet" href="{{ asset('assets/plugins/datatables/dataTables.bootstrap4.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/plugins/datatables/buttons.bootstrap4.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/plugins/datatables/responsive.bootstrap4.min.css') }}">
 
-  <link href="{{asset('assets')}}/css/bootstrap.min.css" rel="stylesheet" type="text/css">
-  <link href="{{asset('assets')}}/css/metismenu.min.css" rel="stylesheet" type="text/css">
-  <link href="{{asset('assets')}}/css/icons.css" rel="stylesheet" type="text/css">
-  <link href="{{asset('assets')}}/css/style.css" rel="stylesheet" type="text/css">
+    {{-- Core styles --}}
+    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/metismenu.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/icons.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
 @endsection
 
 @section('PageContent')
-
 <div class="container-fluid">
+
+    {{-- Page heading --}}
     <div class="page-title-box">
         <div class="row align-items-center">
-          <div class="col-md-8">
-            <div class="page-title-box">
-                <h4 class="page-title">Dashboard</h4>
+            <div class="col-md-8">
+                <h4 class="page-title mb-0">Dashboard</h4>
             </div>
-          </div>
         </div>
     </div>
 
-    <!-- Empty Dashboard Content -->
+    {{-- Dashboard placeholder --}}
     <div class="row">
         <div class="col-12">
-            <div class="card text-center">
-                <div class="card-body">
-                    <h5 class="text-muted">Welcome to your Dashboard</h5>
-                    <p class="mb-0">You can add new widgets, analytics, or reports here.</p>
+            <div class="card">
+                <div class="card-body text-center">
+                    <h5 class="text-muted mb-2">Welcome to the Dashboard</h5>
+                    <p class="mb-0">
+                        This area can be customized with widgets, charts, or reports.
+                    </p>
                 </div>
             </div>
         </div>
     </div>
-</div>
 
+</div>
 @endsection
 
 @section('FooterAssets')
 
-    <!-- Required datatable js -->
+    {{-- jQuery --}}
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="{{asset('assets')}}/plugins/datatables/jquery.dataTables.min.js"></script>
-    <script src="{{asset('assets')}}/plugins/datatables/dataTables.bootstrap4.min.js"></script>
-    <script src="{{asset('assets')}}/plugins/datatables/dataTables.responsive.min.js"></script>
-    <script src="{{asset('assets')}}/plugins/datatables/responsive.bootstrap4.min.js"></script>
 
-    <!-- App js -->
-    <script src="{{asset('assets')}}/js/app.js"></script>
+    {{-- DataTables scripts --}}
+    <script src="{{ asset('assets/plugins/datatables/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('assets/plugins/datatables/dataTables.bootstrap4.min.js') }}"></script>
+    <script src="{{ asset('assets/plugins/datatables/dataTables.responsive.min.js') }}"></script>
+    <script src="{{ asset('assets/plugins/datatables/responsive.bootstrap4.min.js') }}"></script>
+
+    {{-- Application script --}}
+    <script src="{{ asset('assets/js/app.js') }}"></script>
 
 @endsection
